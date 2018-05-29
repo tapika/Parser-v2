64d8c5a77e311bd1fee5ab13d8b87508065b513e
Jenna Kanerva <jmnybl@utu.fi>
2018-05-29 11:44:33 +0300

    morpho transfer script

--- parser/scripts/transfer_morpho.py
+++ parser/scripts/transfer_morpho.py
@@ -22,12 +22,12 @@ def read_conllu(f):
 
 
 
-def transfer(token):
+def transfer_token(token):
     new_xpos="XPOS={x}|{feat}".format(x=token[XPOS],feat=token[FEAT])
     token[XPOS]=new_xpos
     return token
 
-def detransfer(token):
+def detransfer_token(token):
     if token[XPOS]=="_": # must be reinserted multiwordtoken
         token[FEAT]="_" # make sure it does not leak information
         return token
@@ -40,15 +40,30 @@ def detransfer(token):
     token[FEAT]=feat
     return token
 
+def process_batch(data, detransfer=True):
+
+    lines=[]
+    for comm,sent in read_conllu(data.split("\n")):
+        for c in comm:
+            lines.append(c)
+        for token in sent:
+            if detransfer==True:
+                token=detransfer_token(token)
+            else:
+                token=transfer_token(token)
+            lines.append("\t".join(token))
+        lines.append("")
+    return("\n".join(lines))
+
 def main(args):
     for comm, sent in read_conllu(sys.stdin):
         for c in comm:
             print(c)
         for token in sent:
             if args.detransfer:
-                token=detransfer(token)
+                token=detransfer_token(token)
             else:
-                token=transfer(token)
+                token=transfer_token(token)
             print("\t".join(token))
         print()
 
