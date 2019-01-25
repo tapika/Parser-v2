f54f653e75206525b57f8cfc266c09a5e182636b
Jenna Kanerva <jmnybl@utu.fi>
2019-01-25 12:28:51 +0200

    few modification in order to be able to train as part of the parsing pipeline

--- nparser/network.py
+++ nparser/network.py
@@ -107,7 +107,6 @@ class Network(Configurable):
   #=============================================================
   def train(self, load=False):
     """ """
-    
     # prep the configurables
     self.add_file_vocabs(self.parse_files)
     self.setup_vocabs()
